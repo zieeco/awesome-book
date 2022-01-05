bd813be73ddec5886208bdf0cddea7f43bb269f4
zieeco <zieecosam@gmail.com>
2022-01-05 13:28:35 +0100

    Update the page to be interactive on clicking the links

--- index.js
+++ index.js
@@ -97,3 +97,30 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+const seeListsOfBooks = document.getElementById('list-of-books');
+const addNewBook = document.getElementById('add-new-link');
+const contactLink = document.getElementById('contact-link');
+
+seeListsOfBooks.addEventListener('click', () => {
+  seeListsOfBooks.classList.add('active');
+  addNewBook.classList.remove('active');
+  contactLink.classList.remove('active');
+  document.getElementById('awesome-books').classList.remove('hide');
+  document.getElementById('add-book').classList.add('hide');
+  document.getElementById('contact-us').classList.add('hide');
+});
+
+addNewBook.addEventListener('click', () => {
+  seeListsOfBooks.classList.remove('active');
+  addNewBook.classList.add('active');
+  contactLink.classList.remove('active');
+  document.getElementById('add-book').classList.remove('hide');
+  document.getElementById('awesome-books').classList.add('hide');
+});
+
+contactLink.addEventListener('click', () => {
+  seeListsOfBooks.classList.remove('active');
+  addNewBook.classList.remove('active');
+  contactLink.classList.add('active');
+});
